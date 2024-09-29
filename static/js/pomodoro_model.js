31ea7d034f2ec5f5854a17ae8ed5326d88291923
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2024-09-29 14:02:43 +0000

    Refactor: Major modification.

---
+++ static/js/pomodoro_model.js
@@ -0,0 +1,109 @@
+export class PomodoroModel {
+  #workDuration;
+  #shortBreakDuration;
+  #longBreakDuration;
+  #MAX_CYCLES;
+
+  #pubsub;
+
+  #timer;
+  #remainingTime;
+  #currentCycles;
+  #status;
+
+  constructor(config, pubsub) {
+    this.#workDuration = config.workDuration;
+    this.#shortBreakDuration = config.shortBreakDuration;
+    this.#longBreakDuration = config.longBreakDuration;
+    this.#MAX_CYCLES = 4;
+
+    this.#pubsub = pubsub;
+
+    this.#timer = null;
+    this.#remainingTime = this.#workDuration;
+    this.#currentCycles = 0;
+    this.#status = {
+      isWorkTime: false,
+      isBreak: false,
+      isPaused: false,
+      isReset: false,
+    };
+  }
+
+  #timerFinished() {
+    this.#pubsub.publish("timer-finished", this.#getStatus());
+
+    if (this.#status.isWorkTime) {
+      this.#currentCycles++;
+      this.#status.isWorkTime = false;
+      this.#status.isBreak = true;
+      if (this.#currentCycles < this.#MAX_CYCLES) {
+        this.#remainingTime = this.#shortBreakDuration;
+      } else {
+        this.#remainingTime = this.#longBreakDuration;
+      }
+    } else {
+      this.#status.isWorkTime = true;
+      this.#status.isBreak = false;
+      this.#remainingTime = this.#workDuration;
+    }
+  }
+
+  #tick() {
+    this.#remainingTime--;
+    if (this.#remainingTime > 0) {
+      this.#pubsub.publish("timer-tick", this.#getStatus());
+    } else {
+      this.#timerFinished();
+    }
+  }
+
+  startTimer() {
+    this.#status.isWorkTime = true;
+    this.#status.isReset = false;
+
+    this.#pubsub.publish("timer-start", this.#getStatus());
+
+    this.#timer = setInterval(() => this.#tick(), 1000);
+  }
+
+  pauseTimer() {
+    clearInterval(this.#timer);
+    this.#timer = null;
+
+    this.#status.isPaused = true;
+
+    this.#pubsub.publish("timer-paused", this.#getStatus());
+  }
+
+  resumeTimer() {
+    this.#status.isPaused = false;
+
+    this.#pubsub.publish("timer-resumed", this.#getStatus());
+
+    this.#timer = setInterval(() => this.#tick(), 1000);
+  }
+
+  resetTimer() {
+    clearInterval(this.#timer);
+    this.#timer = null;
+    this.#remainingTime = this.#workDuration;
+    this.#currentCycles = 0;
+
+    this.#status.isWorkTime = false;
+    this.#status.isPaused = false;
+    this.#status.isBreak = false;
+    this.#status.isReset = true;
+
+    this.#pubsub.publish("timer-reset", this.#getStatus());
+  }
+
+  #getStatus() {
+    return {
+      remainingTime: this.#remainingTime,
+      currentCycles: this.#currentCycles,
+      maxCycles: this.#MAX_CYCLES,
+      ...this.#status,
+    };
+  }
+}
