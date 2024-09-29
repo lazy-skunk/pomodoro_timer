c3b4355d5896d6e83e5187e7ac8c4d0f9521d8d6
Hirohisa Honmo <65598223+HirohisaHonmo@users.noreply.github.com>
2024-09-29 14:03:41 +0000

    Refactor: Major modification.

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,24 @@
-import { PomodoroUI } from "./pomodoroUI.js";
-import { PomodoroTimer } from "./pomodoroTimer.js";
+import { PubSub } from "./pub_sub.js";
+import { PomodoroModel } from "./pomodoro_model.js";
+import { PomodoroUI } from "./pomodoro_ui.js";
+import { PomodoroController } from "./pomodoro_controller.js";
 import { timerConfig, audioConfig } from "./config.js";
 
+const pubsub = new PubSub();
+
+const alarm = new Audio(audioConfig.alarmUrl);
+const lowAlarm = new Audio(audioConfig.lowAlarmUrl);
+
+const pomodoroUI = new PomodoroUI(alarm, lowAlarm);
+
+const pomodoroModel = new PomodoroModel(timerConfig, pubsub);
+
+const pomodoroController = new PomodoroController(
+  pomodoroModel,
+  pomodoroUI,
+  pubsub
+);
+
 document.addEventListener("DOMContentLoaded", () => {
-  const pomodoroUI = new PomodoroUI();
-
-  const alarm = new Audio(audioConfig.alarmUrl);
-  const lowAlarm = new Audio(audioConfig.lowAlarmUrl);
-
-  const pomodoroTimer = new PomodoroTimer(
-    pomodoroUI,
-    alarm,
-    lowAlarm,
-    timerConfig
-  );
-
-  pomodoroTimer.initialize();
+  pomodoroController.initialize();
 });
